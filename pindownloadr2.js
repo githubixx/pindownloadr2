8f3b65998c75c45d96ee5949c6f01c6ade902bac
Robert Wimmer <robert.wimmer@tiekinetix.com>
2015-08-06 22:15:06 +0200

    completely new implementation of pindownloadr2.(py|js) - necessary because pinterest now makes heavy usage of JavaScript

--- pindownloadr2.js
+++ pindownloadr2.js
@@ -1,90 +1,227 @@
-// Global variable to store the image urls
-var links = [];
+//
+// Imports
+//
+var fs = require('fs');
 
-// Pinterest board url
-var board_url = "";
 
-/**
-Size of the browser i.e. viewport. I know this is ugly but it works 
-if you fetch boards with less than 5000 images. The more images the
-longer it takes to "render" the viewport. 2000 images takes about 
-2 min. to create the viewport... I'm to lazy to implement a "scroller" ;-)
-**/
-var view_width = 2880, view_height = 200000;
+//
+// Some default variables
+//
+var log_level = 'error';
+var verbose = false;
+var viewport_width = 1280;
+var viewport_height = 960;
+var login_url = 'https://www.pinterest.com/login/';
+var user_agent = 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0';
 
-/**
-Load casper with defined viewport and user-agent.
-**/
+var uri = '';
+var loginname = '';
+var loginpw = '';
+var timeout = 6000;
+
+
+//
+// CasperJS defaults
+//
 var casper = require('casper').create({
-    logLevel: 'error',
-    verbose: true,
-    viewportSize: {width: view_width, height: view_height},
-    pageSettings: {
-        userAgent: "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:22.0) Gecko/20100101 Firefox/22.0"
-    }
+    viewportSize: {width: viewport_width, height: viewport_height},
+    logLevel: log_level,
+    verbose: verbose
 });
 
-/**
-Commandline options
-**/
 
-// Don't need this
-casper.cli.drop("cli");
-casper.cli.drop("casper-path");
+//
+// Removing default options passed by the Python executable
+//
+casper.cli.drop('cli');
+casper.cli.drop('casper-path');
 
-// Any arguments options passed?
-if (Object.keys(casper.cli.options).length === 0) {
-    casper.echo("Usage: --url=<pinterest_board_url>").exit();
-}
 
-// Get URL of pinterest board
-if (casper.cli.has("url")) {
-    board_url = casper.cli.get("url");
-} else {
-    casper.echo("No pinterest board url given!").exit();
-}
 
-/**
-Get all images who's parent is a div. This gives you links to images like this
-http://media-cache-ec0.pinimg.com/236x/b0/a1/0f/b0a10fac33057b48a2587763eb861606.jpg
-If you change 236x to 736x you get the links to bigger images:
-http://media-cache-ec0.pinimg.com/736x/b0/a1/0f/b0a10fac33057b48a2587763eb861606.jpg
-And to fetch the originals replace 236x with originals:
-http://media-cache-ec0.pinimg.com/originals/b0/a1/0f/b0a10fac33057b48a2587763eb861606.jpg
-**/
-function getImageLinks() {
-    var links = document.querySelectorAll('div>img');
-    return Array.prototype.map.call(links, function(e) {
-        return e.getAttribute('src');
+//
+// Print out messages in headless browser context.
+// Only needed for debugging...
+//
+
+if(casper.cli.has("debug_output")) {
+    casper.on('remote.message', function(msg) {
+        this.echo('remote message caught: ' + msg);
+    });
+
+    casper.on( 'page.error', function (msg, trace) {
+        this.echo( 'Error: ' + msg, 'ERROR' );
     });
 }
 
-/**
-Open the board url..
-**/
-casper.start(board_url);
 
-/**
-... and evaluate the page.
-**/
-casper.then(function() {
-    links = this.evaluate(getImageLinks);
+//
+// At least we need a URI, Pinterest login name and password and where to save the cookie file
+//
+if (Object.keys(casper.cli.options).length < 3) {
+    casper.echo("Need more options!");
+    casper.echo("");
+	casper.echo("Example : casperjs pindownloadr2.js --uri=uri                              [required, e.g. /jaymenicoleh/future-home/]");
+	casper.echo("                                    --loginname=loginname                  [required]");
+	casper.echo("                                    --loginpw=loginpassword                [required]");
+	casper.echo("                                    --cookiefile=/path/to/cookiefile       [required]");
+    casper.echo("");
+    casper.echo("INFO: Does not work with Facebook login!");
+    casper.echo("");
+	casper.exit();
+}
+
+//
+// Set the useragent we use to connect to pinterest.com.
+//
+casper.userAgent(user_agent);
+
+//
+// Get CLI options
+//
+if(casper.cli.has("uri")) {
+    var uri = casper.cli.get("uri");
+} else {
+    casper.echo("Missing path to board (--uri)!");
+    casper.exit();
+}
+
+if(casper.cli.has("loginname")) {
+    var loginname = casper.cli.get("loginname");
+} else {
+    casper.echo("Missing login (--loginname)!")
+    casper.exit();
+}
+
+if(casper.cli.has("loginpw")) {
+    var loginpw = casper.cli.get("loginpw");
+} else {
+    casper.echo("Missing login password (--loginpw)!");
+    casper.exit();
+}
+
+if(casper.cli.has("cookiefile")) {
+    var cookiefile = casper.cli.get("cookiefile");
+} else {
+    casper.echo("Missing cookiefile (--cookiefile)!");
+    casper.exit();
+}
+
+//
+// Read cookie file if it exists
+//
+if (fs.exists(cookiefile)) {
+    var cookies = fs.read(cookiefile);
+    phantom.cookies = JSON.parse(cookies);
+
+    // Directly request uri to download
+    casper.start('https://www.pinterest.com' + uri, function() {
+        var html = this.getHTML();
+    });
+} else {
+    // Without cookie file we first need to login
+    casper.start(login_url, function() {
+        this.fill('body > div.App.AppBase.Module.content_only > div.appContent > div.mainContainer > div > div > div > form', {
+            username_or_email: loginname,
+            password: loginpw
+        }, true);
+    });
+
+    casper.thenOpen('https://www.pinterest.com' + uri, function() {
+        var html = this.getHTML();
+
+        // Save cookies after login
+        var cookies = JSON.stringify(phantom.cookies);
+        fs.write(cookiefile, cookies, 644);
+    });
+
+}
+
+
+casper.thenEvaluate(function() {
+
+    // This reenables logging to console in headless browser context
+    delete console.log;
+
+    var pTimerCounter = 1;
+    var pLastCount = 0;
+    var pPins = new Array();
+    var pPinsIndexes = new Array();
+    window.done = 0;
+    window.data = new Array();
+
+    var pTimer = window.setInterval(function() {
+        
+        var pUrls = $('.pinImageWrapper');
+        var pDescriptions = $('.pinImg');
+        var pBoard = $('.creditTitle');
+	    var pBoardUri = $('.creditItem > a');
+
+        pLength = parseInt($('.padItems').css('height').replace('px', ''));
+
+        if (pLength == pLastCount) {
+            window.clearInterval(pTimer);
+            window.done = 1;
+        } else {
+            for ( var i=0 ; i < pUrls.length ; i++) {
+                var pPin = {};
+                pPin['pin_page'] = 'http://www.pinterest.com'+$(pUrls[i]).attr('href');
+                pPin['board'] = $(pBoard[i]).text();
+                pPin['description'] = $(pDescriptions[i]).attr('alt');
+                pPin['pin_thumbnail'] = $(pDescriptions[i]).attr('src');
+		        pPin['pin_board_uri'] = $(pBoardUri[i]).attr('href');
+
+                // search if pin already pushed
+                if (!(pPin['pin_page'] in pPinsIndexes)) {
+                    window.data.push(pPin); 
+                    pPinsIndexes[pPin['pin_page']] = '';
+                }
+            }
+
+            pLastCount = pLength;
+
+            //
+            // The following codelines doesn't work with headless (PhantomJS) browser (but some with SlimerJS).
+            // If someone knows how to scroll to next page with PhantomJS/CasperJS please
+            // send a pull request ;-) It seems that the window object doesn't exist in PhantomJS.
+            //
+            // window.document.body.scrollTop = document.body.scrollHeight;
+            // window.scrollTo(0,document.body.scrollHeight || document.documentElement.scrollHeight);
+            // window.scrollTo(0,document.body.scrollHeight);
+
+            // Works only with Firefox (SlimerJS) but isn't very nice.
+            // window.scrollByPages(1);
+
+            // This finally works with SlimerJS (Gecko) and CasperJS scrollTo function. That's the reason we
+            // use SlimerJS instead of PhantomJS.
+            this.scrollTo(0,document.body.scrollHeight);
+            pTimerCounter++;
+
+        }
+    }, 3000);  // use high values like 6000 ms to wait for next page
+               // to reduce possibility that fetching images stops to early.
+               // This value depends on your wire speed and the response time
+               // from pinterest.com.
 });
 
-/**
-Print the result.
-**/
-casper.then(function() {
-    // this.echo(links.length + ' links found:');
-    links_count = links.length;
-    for (var i = 0; i < links_count; i++) {
-        this.echo(links[i]);
-    }    
-});
+//
+// Wait for global variable "done" and print collected image uri's
+//
+casper.waitFor(function() {
+    return this.getGlobal('done') === 1;
+    }, function() {
 
-/**
-Say good night, Gracie ;-)
-**/
-casper.run(function() {
+        pins = this.getGlobal('data');
+        board = new Array();
+
+        pins_count = pins.length;
+        for (var i = 0; i < pins_count; i++) {
+            pin = pins[i];
+	        this.echo(pin['pin_thumbnail']);
+        }
+
     this.exit();
-});
+    }, function timeout() {
+
+    }, 1800000); // allow timeout of 30 minutes to execute the script...
+
+casper.run();
--- pindownloadr2.py
+++ pindownloadr2.py
@@ -1,23 +1,60 @@
+#!/usr/bin/env python
+
 import requests
 import os
 import errno
 import sys
 import argparse
+import collections
+import itertools
 import subprocess as sp
+import shutil
+import locale
 
-from PIL import Image
-from StringIO import StringIO
+import logging
 
-# Images found
-images_found = 0
+from os.path import expanduser
+
+try:
+    # Python 3
+    import configparser as cp
+except ImportError:
+    # Python 2
+    import ConfigParser as cp
+
+try:
+    # Python 3
+    from urllib.request import urlopen
+except ImportError:
+    # Python 2
+    from urllib2 import urlopen
+
+try:
+    # Python 3
+    import http.client as http_client
+except ImportError:
+    # Python 2
+    import httplib as http_client
+
+# Enables requests lib debugging.
+# Remove """ if needed
+"""
+http_client.HTTPConnection.debuglevel = 1
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+"""
 
 # HTTP Header for our "browser"
-http_request_header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:22.0) Gecko/20100101 Firefox/22.0',
-                       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+http_request_header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0',
                        'Accept-Encoding': 'gzip, deflate',
                        'Accept-Language': 'en-US,en;q=0.5'}
 
+
 def ensure_save_path(save_path):
+    """Ensure that the path to save directories and pictures in exists."""
     try:
         os.makedirs(save_path)
     except OSError as exc:  # Python >2.5
@@ -26,122 +63,130 @@ def ensure_save_path(save_path):
         else:
             raise
 
-def generate_save_path(save_path, board_url):
-    split_path = board_url.split(os.sep)
-    split_path.pop()
-    board_name = split_path.pop()
-    pinterest_user = split_path.pop()
-    return os.path.join(save_path, pinterest_user, board_name)
 
-def download_image(url, save_path='/tmp'):
-    '''
-    Download a image.
-    '''
-    # Get image name from url
-    image_name = url.rsplit("/",1)[1]
+def download_image(url, download_dir='/tmp'):
+    """Download the image."""
 
-    # Change path to point the originals
-    new_url = url.replace("236x", "originals")
+    # Get image name from url and remove whitespace/newline at the end
+    image_name = url.rsplit("/", 1)[1]
+
+    # Change path to point the originals (the big pictures instead of the small ones...)
+    originals_url = url.replace("236x", "originals")
 
     # Check if image already exists
-    image_name_tmp = image_name.split(".",1)[0]
+    image_path = download_dir + "/" + image_name
+    if os.path.exists(image_path):
+        print("Skipping " + image_path + ". File exists!")
+        return
 
-    image_extensions = ["jpg", "gif", "png"]
-    for extension in image_extensions:
-        image_path = save_path + "/" + image_name_tmp + "." + extension
-        if os.path.exists(image_path):
-            print("Skipping " + image_name_tmp + "." + extension + ". File exists!")
-            return
+    r = requests.get(originals_url, headers=http_request_header, stream=True)
+    with open(image_path, 'wb') as out_file:
+        shutil.copyfileobj(r.raw, out_file)
+    del r
 
-    print("Downloading " + image_name)
-    r = requests.get(new_url, headers=http_request_header)
 
-    i = Image.open(StringIO(r.content))
+def casperjs_output_as_list(uri, loginname, loginpw, cookiepath):
+    """Read CasperJS output and add every line it to a python list."""
 
-    # We need to do this because the image extension says us all
-    # images are JPEG but some images are GIF, PNG, ...
-    if (i.format == "JPEG"):
-        i.save(os.path.join(save_path, image_name))
-    elif (i.format == "GIF"):
-        image_name = image_name.replace(".jpg", ".gif")
-        i.save(os.path.join(save_path, image_name))
-    elif (i.format == "PNG"):
-        image_name = image_name.replace(".jpg", ".png")
-        i.save(os.path.join(save_path, image_name))
-    else:
-        print("Skipping " + new_url + ". Unknown format!")
+    _urls = []
 
-def casperjs_output_as_list(url):
-    '''
-    Read output of links from CasperJS and add it into a python list.
-    '''
-    link_list = []
-    p = sp.Popen(["/usr/local/bin/casperjs", "/usr/local/bin/pindownloadr2.js", "--url=" + url], stdin=sp.PIPE, stdout=sp.PIPE, close_fds=True)
-    (stdout, stdin) = (p.stdout, p.stdin)
-    data = stdout.readline()
-    while data:
-        data = stdout.readline().rstrip('\n')
-        link_list.append(data)
-    return link_list
+    _p = sp.Popen(["casperjs", "--engine=slimerjs", "/usr/local/bin/pindownloadr2.js", "--uri="+uri,
+                   "--loginname="+loginname, "--loginpw="+loginpw, "--cookiefile="+cookiepath],
+                   stdin=sp.PIPE, stdout=sp.PIPE, close_fds=True)
+    _output = _p.stdout.readlines()
+
+    for _url in _output:
+        # convert to unicode string, using locale.getpreferredencoding() and remove newline character
+        _urls.append(_url.decode(locale.getpreferredencoding()).rstrip())
+
+    return _urls
+
+
+def config_section_map(section):
+    """Read configuration options."""
+
+    dict1 = {}
+    options = config.options(section)
+    for option in options:
+        try:
+            dict1[option] = config.get(section, option)
+        except:
+            dict1[option] = None
+    return dict1
+
 
 if __name__ == "__main__":
 
     # Parse commandline options
     ap = argparse.ArgumentParser(description="Fetch pinterest images. USE THIS SCRIPT CAREFULLY!")
-    ap.add_argument('--url', '-u', dest='board_url', help='The board url to download (e.g. /<username>/<boardname>/)')
-    ap.add_argument('--path', '-p', dest='save_path', help='Directory to save the pictures.')
-    ap.add_argument('--update', dest='save_path', help='The path to update.')
+    ap.add_argument('--uri',    '-u', dest='uri',     help='The uri (e.g. https://www.pinterest.com/jaymenicoleh/future-home/ - \
+                                                            /jaymenicoleh/future-home/ is the one you need.')
+    ap.add_argument('--path',   '-p', dest='path',    help='Directory to save the pictures. If directory /path/user/board/ \
+                                                            exists the script will only download pictures that does \
+                                                            not exist (which is basically "only get new pictures".')
+    ap.add_argument('--config', '-c', dest='config',  help='Path to config file (login data for pinterest.com \
+                                                            (defaults to $HOME/.pindownloadr)')
     args = ap.parse_args()
 
-    # Board url
-    if args.board_url is not None:
-        board_url = args.board_url[0]
-        if not board_url.endswith("/"):
-            board_url = board_url + "/"
-        if board_url.startswith("http:"):
-            print("Only /<username>/<boardname>/ for url argument please!")
-            sys.exit(1)
-
-    if args.save_path is not None:
-        save_path_tmp=args.save_path
+    # Uri
+    if args.uri is not None:
+        uri = args.uri
     else:
-        print("No path to save given!")
+        print("No URI provided!")
         sys.exit(1)
 
-    if save_path_tmp is not None and args.board_url is None:
-        _update_path_tmp = save_path_tmp.split("/")
-        if len(_update_path_tmp) < 4:
-            print("Update path needs at least two path elements e.g. /user/board/ !")
-            sys.exit(1)
-        else:
-            _user = _update_path_tmp[len(_update_path_tmp) - 3]
-            _board = _update_path_tmp[len(_update_path_tmp) - 2]
-            board_url = "/" + _user + "/" + _board + "/"
-            save_path_tmp=save_path_tmp.replace(board_url, "")
-
-    # Generate path for pictures
-    save_path = generate_save_path(save_path_tmp, board_url)
-
-    # Ensure that picture directory exists
-    ensure_save_path(save_path)
-
-    print("")
-    print("Fetching image links... This could take some time (depends on your wire speed)!")
-    print("")
-
-    # Read in links to 236x images from CasperJS
-    casper_links = casperjs_output_as_list("http://www.pinterest.com" + board_url)
-
-    print("Found " + str(len(casper_links)) + " images. Starting download...")
-
-    # Board deleted?
-    if len(casper_links) < 2:
-        print("Seems that this board was deleted or other problem!")
+    # Where to store pictures?
+    if args.path is not None:
+        download_dir = args.path + uri
+    else:
+        print("No path where to save pictures given!")
         sys.exit(1)
+
+    # Config file
+    if args.config is not None:
+        config_file = os.path.join(args.config, '.pindownloadr/config')
+        cookiepath = os.path.join(args.config, '.pindownloadr/cookie')
+    else:
+        config_file = os.path.join(expanduser("~"), '.pindownloadr/config')
+        cookiepath = os.path.join(expanduser("~"), '.pindownloadr/cookie')
+
+    # Load config
+    config = cp.ConfigParser()
+    config.read(config_file)
+    loginname = config_section_map('login')['loginname']
+    loginpw = config_section_map('login')['loginpw']
+
+    # Check config values
+    if loginname is None:
+        print ("No loginname in configuration file found!")
+    if loginpw is None:
+        print ("No loginpw (login password) in configuration file found!")
+
+    print("")
+    print("Fetching image links... This could take some time (depends mostly on your wire speed)!")
+    print("")
+
+    # Read links from CasperJS output
+    casperjs_links = casperjs_output_as_list(uri, loginname, loginpw, cookiepath)
+
+    # Any pins (maybe board deleted...)?
+    if len(casperjs_links) < 2:
+        print("Seems that this board was deleted or other problem! Output from CasperJS:")
+        print()
+        print(casperjs_links)
+        print()
+        sys.exit(1)
+
+    print("Found " + str(len(casperjs_links)) + " images. Starting download...")
+
+    # Create directory where to save pictures
+    ensure_save_path(download_dir)
 
     # Now fetch original images
-    for image_link in casper_links:
-        if "/236x" in image_link:
-            download_image(image_link, save_path)
+    for picture_url in casperjs_links:
+
+        if picture_url.find("/236x") != -1:
+            download_image(picture_url, download_dir)
+
         else:
-            print("Skipping URL: " + image_link)
+            print("Skipping URL: " + picture_url)
