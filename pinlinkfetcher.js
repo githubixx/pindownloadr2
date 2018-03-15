17ceb51f086cd893a019129df240e5e962d2c0cd
githubixx <home@tauceti.net>
2018-03-15 20:18:51 +0100

    adjust pinlinkfetcher.js to reflect changes in UI

--- pinlinkfetcher.js
+++ pinlinkfetcher.js
@@ -28,10 +28,10 @@ const viewportHeight = 960;
 const domainPrefix = 'https://www.pinterest.';
 
 // Height of the current page in pixel (from CSS property)
-const selectorCssHeight = '._qj._2f';
+const selectorCssHeight = '._qj._2h';
 // The preview pictures have a "class" attribute with this value.
 // The IMG tag contains a SRC attribute we fetch for every pic.
-const selectorPreviewPictures = '._mj._25._3v._2h';
+const selectorPreviewPictures = '._mj._27._3x._2j';
 
 
 async function run(req,res) {
@@ -96,13 +96,13 @@ async function scrape(chromeless) {
       var previousHeight = 0;
 
       /* CSS selector for the height of the current page in pixel (from CSS property) */
-      var selectorCssHeight = '._qj._2f';
+      var selectorCssHeight = '._qj._2h';
 
       /*
        * The preview pictures have a "class" attribute with this value.
        * The IMG tag contains a SRC attribute we fetch for every pic.
        */
-      var selectorPreviewPictures = '._mj._25._3v._2h';
+      var selectorPreviewPictures = '._mj._27._3x._2j';
 
       /* Scroll to next page every ... */
       var scrollInterval = 6000;
